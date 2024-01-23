75263f26793cf12b7bfd5c0fe566cb315f5d030b
shiori123 <shioo5555@gmail.com>
2024-01-23 22:02:57 +0900

    userテーブルとsubjectテーブルを多対多でリレーション

--- app/Models/Users/User.php
+++ app/Models/Users/User.php
@@ -66,8 +66,9 @@ class User extends Authenticatable
         return $this->belongsToMany('App\Models\Calendars\ReserveSettings', 'reserve_setting_users', 'user_id', 'reserve_setting_id')->withPivot('id');
     }
 
+    // ユーザー
     public function subjects(){
-        return ;// リレーションの定義
+        return $this->belongsToMany(Subjects::class, 'subject_users', 'user_id', 'subject_id');// リレーションの定義
     }
 
     // いいねしているかどうか
