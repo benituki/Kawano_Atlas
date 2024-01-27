d9a462d24e16a936c3e72a5a955e18679291eb55
shiori123 <shioo5555@gmail.com>
2024-01-27 21:24:28 +0900

    postsとsub_categoriesの多対多のリレーション、main_categoriesとsub_categoriesの一対多のリレーション設定

--- app/Models/Categories/SubCategory.php
+++ app/Models/Categories/SubCategory.php
@@ -3,6 +3,8 @@
 namespace App\Models\Categories;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Categories\MainCategory;
+use App\Models\Posts\Post;
 
 class SubCategory extends Model
 {
@@ -14,9 +16,11 @@ class SubCategory extends Model
     ];
     public function mainCategory(){
         // リレーションの定義
+        return $this->belongsTo(MainCategory::class);
     }
 
     public function posts(){
         // リレーションの定義
+        return $this->belongsToMany(Post::class, 'post_sub_categories', 'sub_category_id', 'post_id' );
     }
 }
