16bc63b44bd81c485c32dc90c0c30a508f721eb3
shiori123 <shioo5555@gmail.com>
2024-01-30 15:43:24 +0900

    いいねカウントメソッドをcontrollerからモデルへ移動。

--- app/Models/Posts/Like.php
+++ app/Models/Posts/Like.php
@@ -14,6 +14,7 @@ class Like extends Model
     ];
 
     public function likeCounts($post_id){
-        return $this->where('like_post_id', $post_id)->get()->count();
+        return Like::where('like_post_id', $post_id)->count();
     }
+    
 }
