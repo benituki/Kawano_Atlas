6c116ff189c4267dc5ffcb7687654fb1f064a320
shiori123 <shioo5555@gmail.com>
2024-02-08 18:27:46 +0900

    どのように作用されているのかを記述（スクール内）

--- app/Searchs/SearchResultFactories.php
+++ app/Searchs/SearchResultFactories.php
@@ -7,21 +7,21 @@ class SearchResultFactories{
 
   // 改修課題：選択科目の検索機能
   public function initializeUsers($keyword, $category, $updown, $gender, $role, $subjects){
-    if($category == 'name'){
-      if(is_null($subjects)){
+    if($category == 'name'){ //条件：$categoryがnameだったら＝カテゴリが名前と選択されていたら
+      if(is_null($subjects)){ //条件：$subjectsがnullだったら＝サブジェクトが何も選択されていなければ
         $searchResults = new SelectNames();
-      }else{
-        $searchResults = new SelectNameDetails();
+      }else{ //条件：それ以外＝サブジェクトが選択されていたら。
+        $searchResults = new SelectNameDetails(); //ここが原因
       }
       return $searchResults->resultUsers($keyword, $category, $updown, $gender, $role, $subjects);
-    }else if($category == 'id'){
+    }else if($category == 'id'){ //条件：$categoryがidだったら＝カテゴリが社員IDと選択されていたら
       if(is_null($subjects)){
         $searchResults = new SelectIds();
       }else{
-        $searchResults = new SelectIdDetails();
+        $searchResults = new SelectIdDetails(); //ここが原因
       }
       return $searchResults->resultUsers($keyword, $category, $updown, $gender, $role, $subjects);
-    }else{
+    }else{ //それ以外＝何も選択されていなかったら
       $allUsers = new AllUsers();
     return $allUsers->resultUsers($keyword, $category, $updown, $gender, $role, $subjects);
     }
