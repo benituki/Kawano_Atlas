c223d9cd281a183bf1d36352ad6792e49ce445e9
shiori123 <shioo5555@gmail.com>
2024-03-20 16:50:26 +0900

    スクール予約確認画面：バックグランドカラー、影追加。大きさ変更

--- resources/views/authenticated/calendar/admin/calendar.blade.php
+++ resources/views/authenticated/calendar/admin/calendar.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
 <div class="w-75 m-auto">
-  <div class="w-100">
-    <p>{{ $calendar->getTitle() }}</p>
+  <div class="w-100 border pt-5 pb-5" style="background-color: #fff; box-shadow: 0 20px 20px rgba(0, 0, 0, 0.16); margin-top: 30px;">
+    <p style="font-size: 20px; text-align: center;">{{ $calendar->getTitle() }}</p>
     <p>{!! $calendar->render() !!}</p>
   </div>
 </div>
