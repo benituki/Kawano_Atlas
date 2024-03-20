0eca879523392008eee2b89bc94a4b89d73441e6
shiori123 <shioo5555@gmail.com>
2024-03-20 16:03:08 +0900

    ユーザー検索の詳細検索のデザインを修正。

--- public/js/user_search.js
+++ public/js/user_search.js
@@ -1,6 +1,9 @@
 $(function () {
   $('.search_conditions').click(function () {
     $('.search_conditions_inner').slideToggle();
+    $(this).find('.toggle_arrow').text(function(_, text) {
+      return text === '∨' ? '∧' : '∨';
+    });
   });
 
   $('.subject_edit_btn').click(function () {
--- resources/views/authenticated/users/search.blade.php
+++ resources/views/authenticated/users/search.blade.php
@@ -55,35 +55,40 @@
     @endforeach
   </div>
   <div class="search_area w-25 border">
-    <div class="">
-      <div>
+    <div class="all_search">
+      <p>検索</p>
+      <div class="search_keyword">
         <input type="text" class="free_word" name="keyword" placeholder="キーワードを検索" form="userSearchRequest">
       </div>
-      <div>
+      <div class="search">
         <lavel>カテゴリ</lavel>
+        <br>
         <select form="userSearchRequest" name="category">
           <option value="name">名前</option>
           <option value="id">社員ID</option>
         </select>
       </div>
-      <div>
+      <div class="search">
         <label>並び替え</label>
+        <br>
         <select name="updown" form="userSearchRequest">
           <option value="ASC">昇順</option>
           <option value="DESC">降順</option>
         </select>
       </div>
-      <div class="">
-        <p class="m-0 search_conditions"><span>検索条件の追加</span></p>
+      <div class="additional">
+        <p class="m-0 search_conditions"><span>検索条件の追加</span><span class="toggle_arrow">∨</span></p>
         <div class="search_conditions_inner">
           <div>
             <label>性別</label>
+            <br>
             <span>男</span><input type="radio" name="sex" value="1" form="userSearchRequest">
             <span>女</span><input type="radio" name="sex" value="2" form="userSearchRequest">
             <span>その他</span><input type="radio" name="sex" value="3" form="userSearchRequest">
           </div>
           <div>
             <label>権限</label>
+            <br>
             <select name="role" form="userSearchRequest" class="engineer">
               <option selected disabled>----</option>
               <option value="1">教師(国語)</option>
@@ -94,16 +99,17 @@
           </div>
           <div class="selected_engineer">
             <label>選択科目</label>
+            <br>
             <span>国語</span><input type="checkbox" name="subject[]" value="1" form="userSearchRequest">
             <span>数学</span><input type="checkbox" name="subject[]" value="2" form="userSearchRequest">
             <span>英語</span><input type="checkbox" name="subject[]" value="3" form="userSearchRequest">
         </div>
       </div>
-      <div>
-        <input type="reset" value="リセット" form="userSearchRequest">
-      </div>
-      <div>
+      <div class="search_btn">
         <input type="submit" name="search_btn" value="検索" form="userSearchRequest">
+      </div>
+      <div class="search_reset">
+        <input type="reset" value="リセット" form="userSearchRequest">
       </div>
     </div>
     <form action="{{ route('user.show') }}" method="get" id="userSearchRequest"></form>
