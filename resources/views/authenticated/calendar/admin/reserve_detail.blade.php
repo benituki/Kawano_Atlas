0751e2e90f30cf8fbdc34424d87a1aac10b76acd
shiori123 <shioo5555@gmail.com>
2024-03-21 16:13:08 +0900

    予約詳細確認画面：リストの背景色を交互になるように設定。

--- resources/views/authenticated/calendar/admin/reserve_detail.blade.php
+++ resources/views/authenticated/calendar/admin/reserve_detail.blade.php
@@ -2,11 +2,11 @@
 
 @section('content')
 <div class="vh-100 d-flex" style="align-items:center; justify-content:center;">
-  <div class="w-50 m-auto h-75">
-    <p><span>{{ $date }}</span><span class="ml-3">{{ $part }}部</span></p>
-    <div class="h-75 border">
-      <table class="">
-        <tr class="text-center">
+  <div class="w-50 m-auto">
+    <p style="font-size: 20px;"><span>{{ $date }}</span><span class="ml-3">{{ $part }}部</span></p>
+    <div class="users-tre" style="background-color: #fff; padding:2%; box-shadow: 0 20px 20px rgba(0, 0, 0, 0.16); border-radius: 10px;">
+      <table class="users-table" style="width: 100%;">
+        <tr class="text-center" style="background-color: #03AAD2;">
           <th class="w-25">ID</th>
           <th class="w-25">名前</th>
           <th class="w-25">場所</th>
